c8264f215e0e4f3032b007cf7154a10d1d0ccc6d
pomelo925 <yoseph.huang@gmail.com>
2025-01-02 20:22:44 +0800

    alter image name

---
+++ docker/gpu/gpu_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+###### Settings ######
+export COMMAND="bin/bash"
+export DISPLAY=:0
+#######################
+
+xhost +local:docker
+docker compose -p actpp-gpu down --volumes --remove-orphans
+docker compose -p actpp-gpu up -d koch-actpp
